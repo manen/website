184d1fb433e3508b5acd2cac203a3cba189bedd6
manen <varga.bercel.i@gmail.com>
2021-02-21 01:22:39 +0100

    nice

---
+++ ssg/posts/data.go
@@ -0,0 +1,34 @@
+package posts
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Data struct {
+	Posts []*PostData `json:"posts"`
+}
+
+type PostData struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var data Data
+
+func init() {
+	bytes, err := ioutil.ReadFile("./posts/data.json")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(bytes))
+
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(data)
+}
--- ssg/posts/posts.go
+++ ssg/posts/posts.go
@@ -1,41 +1,14 @@
 package posts
 
 import (
-	"fmt"
-	"io/ioutil"
-	"path"
-	"strings"
-
 	"github.com/manen/website/ssg/pages"
 )
 
 func ResolvePosts() []*pages.Page {
-	dirStr := "./posts"
-	dir, err := ioutil.ReadDir(dirStr)
-	if err != nil {
-		panic(err)
-	}
-
 	var posts []*pages.Page
-
-	for _, v := range dir {
-		if !strings.HasSuffix(v.Name(), ".md") {
-			fmt.Println("Non-md file caught: " + v.Name())
-			continue
-		}
-
-		pt := path.Join(dirStr, v.Name())
-		posts = append(posts, ResolvePost(pt))
+	for _, post := range data.Posts {
+		posts = append(posts, pages.NewPage(post.Title, post.Author, "uh well now this is unimplemented so"))
 	}
 
 	return posts
-}
-
-func ResolvePost(path string) *pages.Page {
-	bytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-
-	return pages.NewPage("N/A", "N/A", string(bytes))
 }
